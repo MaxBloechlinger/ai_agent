cbe3d57800dd33979e7f6310f41f05b3337d068a
max <max.bloechlinger.job@gmail.com>
2025-07-04 18:32:41 +0200

    new stuff

--- tests.py
+++ tests.py
@@ -1,10 +1,19 @@
-from functions.get_file_content import get_file_content
+from functions.run_python import run_python_file
+
+
+def test():
+    result = run_python_file("calculator", "main.py")
+    print(result)
+
+    result = run_python_file("calculator", "tests.py")
+    print(result)
+
+    result = run_python_file("calculator", "../main.py")
+    print(result)
+
+    result = run_python_file("calculator", "nonexistent.py")
+    print(result)
+
 
 if __name__ == "__main__":
-
-    print(get_file_content("calculator", "main.py"))
-    print()
-    print(get_file_content("calculator", "pkg/calculator.py"))
-    print()
-    print(get_file_content("calculator", "/bin/cat"))
-    print()
+    test()
